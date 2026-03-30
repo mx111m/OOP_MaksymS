443adc465c5f8441ecc11c41773873b103f11802
Maksym Schostak <109462224+mx111m@users.noreply.github.com>
2026-03-31 00:13:24 +0300

    Create Test.java
    додав простий тест для перевірки роботи програми / git push

---
+++ Program3/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        View view = new ViewableResult().getView();
+        view.viewInit();
+        view.viewShow();
+        System.out.println("Test OKAYYYY 100 ITS GOODDDD");
+    }
+}
