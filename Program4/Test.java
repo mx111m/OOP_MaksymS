b1b0786d9dbac3fb88dbec1178e2387413ecf46c
Maksym Schostak <109462224+mx111m@users.noreply.github.com>
2026-03-31 00:16:38 +0300

    Create Test.java
    додав тест для перевірки роботи таблиці

---
+++ Program4/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        View view = new ViewableTable().getView();
+        view.viewInit();
+        view.viewShow();
+
+        System.out.println("test ookkkkk");
+    }
+}
